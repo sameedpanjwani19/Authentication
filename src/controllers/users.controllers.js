223c33e96190f9df192d99ab2015140070ab05dd
abdulsameed19 <160429352+sameedpanjwani19@users.noreply.github.com>
2024-12-06 08:35:35 +0500

    authentication

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,90 @@
+import User from "../models/users.models.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const generateAccessToken = (user) => {
+  return jwt.sign({ email: user.email }, process.env.ACCESS_JWT_SECRET, {
+    expiresIn: "6h",
+  });
+};
+const generateRefreshToken = (user) => {
+  return jwt.sign({ email: user.email }, process.env.REFRESH_JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+// register user
+
+const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) return res.status(400).json({ message: "email required" });
+  if (!password) return res.status(400).json({ message: "password required" });
+
+  const user = await User.findOne({ email: email });
+  if (user) return res.status(401).json({ message: "user already exist" });
+
+  const createUser = await User.create({
+    email,
+    password,
+  });
+  res.json({ message: "user registered successfully", data: createUser });
+};
+
+// login user
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) return res.status(400).json({ message: "email required" });
+  if (!password) return res.status(400).json({ message: "password required" });
+  // email mujood ha bhi ya nahi ha
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ message: "no user found" });
+  // password compare krwayenga bcrypt
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid)
+    return res.status(400).json({ message: "incorrect password" });
+
+  // token generate
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
+
+  // cookies
+  res.cookie("refreshToken", refreshToken, { http: true, secure: false });
+
+  res.json({
+    message: "user loggedIn successfully",
+    accessToken,
+    refreshToken,
+    data: user,
+  });
+};
+
+// logout user
+const logoutUser = async (req, res) => {
+  res.clearCookie("refreshToken");
+  res.json({ message: "user logout successfully" });
+};
+
+// refreshtoken
+const refreshToken = async (req, res) => {
+  const refreshToken = req.cookies.refreshToken || req.body.refreshToken;
+  if (!refreshToken)
+    return res.status(401).json({ message: "no refresh token found!" });
+
+  const decodedToken = jwt.verify(refreshToken, process.env.REFRESH_JWT_SECRET);
+
+  const user = await User.findOne({ email: decodedToken.email });
+
+  if (!user) return res.status(404).json({ message: "invalid token" });
+
+  const generateToken = generateAccessToken(user);
+  res.json({ message: "access token generated", accesstoken: generateToken });
+
+  res.json({ decodedToken });
+};
+
+
+
+export { registerUser, loginUser, logoutUser, refreshToken };
---
+++ src/routes/users.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  loginUser,
+  logoutUser,
+  refreshToken,
+  registerUser,
+} from "../controllers/users.controllers.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/refreshtoken", refreshToken);
+
+export default router;
